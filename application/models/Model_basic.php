20530be8d124c59651b5fb994920bb85efa05de6
dev-wiqi <dev@wiqi.co>
2015-08-10 11:54:05 +0700

    define

---
+++ application/models/Model_basic.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Models_basic extends CI_model {
+    /*
+     * BRP Application V.1.0 Copyright 2015
+     * Build Date : 07 Agustus 2015
+     * Founder & Programmer : Wisnu Groho Aji 
+     * Website : http://wiqi.co
+     */
+    
+    function reff($pages){
+        
+    }
+}
